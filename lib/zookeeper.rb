6f7d51aec2f84ae887dd81bfdebe602489e43c91
Em01 <emma_williams1@hotmail.co.uk>
2015-01-22 14:12:02 +0000

    initial test for zookeeper passes

--- lib/zookeeper.rb
+++ lib/zookeeper.rb
@@ -1,2 +1,5 @@
 class Zookeeper
+
+	def feed(args={})
+	end
 end
--- spec/zookeeper_spec.rb
+++ spec/zookeeper_spec.rb
@@ -1,2 +1,10 @@
+require 'zookeeper'
+
 describe Zookeeper do 
-end
+
+	it "should be able to feed the bamboo to the pandas" do 
+		panda = Panda.new
+		Zookeeper.new.feed(food: :bamboo, animal: panda)
+	end
+	# it "should be able to feed zebras to the lions"
+end 
