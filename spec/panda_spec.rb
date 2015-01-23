be2138cb583166a448cbfb561ea51e922d52b68e
Em01 <emma_williams1@hotmail.co.uk>
2015-01-23 14:58:41 +0000

    animal will eat what we like it to and test coverage is good

--- spec/panda_spec.rb
+++ spec/panda_spec.rb
@@ -15,4 +15,8 @@ describe Panda do
 	it 'should not like grasshoppers' do 
 		expect(Panda.new.likes?("grasshoppers")).to eq(false)
 	end
+
+	it "should be able to eat the food" do 
+		expect(Panda.new.eat(:bamboo)).to be true
+	end
 end 
